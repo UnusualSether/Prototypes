9f56447e017b78765cb917bd20a4b6eab8ec2f78
UnusualSether <lucaspelzel@gmail.com>
2026-02-25 17:21:21 -0300

    State Machine Finished
    OnRailsStateMachine script contains all of the features for the task, inside of the script you will find -
    +Being able to easily transition between states, when calling the AdvanceState function it'll automatically go to the next state which matches the flow of gameplay (Rails -> Encounter -> Cleanup -> Repeat)
    +The beginning and end functions of each state have their own events, meaning other scripts can easily subscribe to the functions and automatically do their desired task when a state begins or ends.
    You cna easilt test this scirpt in the StateMachineTest scene.

---
+++ FallDay/Assets/StateMachine/OnRailsStateMachine.cs
@@ -0,0 +1,127 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using System.Collections;
+
+[Serializable]
+public class OnRailsStateMachine : MonoBehaviour
+{
+    public enum States
+    {
+        OnRail,
+        Encounter,
+        Cleanup
+    }
+    public States currentState;
+
+    public States[] stateFlow = 
+        {
+        States.OnRail,
+        States.Encounter,
+        States.Cleanup
+        };
+
+
+    //State Start Events
+    public event Action RailStarted;
+    public event Action EncounterStarted;
+    public event Action CleanupStarted;
+
+    //State End Events
+    public event Action RailEnded;
+    public event Action EncounterEnded;
+    public event Action CleanupEnded;
+
+    public Dictionary<States, Action> stateToFunction;
+
+    private void Start()
+    {
+        stateToFunction = new Dictionary<States, Action>
+        {
+            {States.OnRail, StartRails },
+            {States.Encounter, StartEncounter },
+            {States.Cleanup, StartCleanup }
+
+        };
+    }
+
+
+    #region Test Purposes
+    public void CheckCurrentState(States stateToCheck)
+    {
+        Debug.Log($"Current state is {stateToCheck}");
+    }
+
+    [ContextMenu("StartTheAutoPlay")]
+    public void StartAutoPlayer()
+    {
+        StartCoroutine(AutoPlayer());
+    }
+
+    IEnumerator AutoPlayer()
+    {
+        yield return new WaitForSeconds(5);
+        AdvanceStateFlow();
+        StartAutoPlayer();
+    }
+    #endregion
+
+    [ContextMenu("AdvanceGameState")]
+    public void AdvanceStateFlow()
+    {
+        //Set the public States to the next state
+        int currentStateIndexer = Array.IndexOf(stateFlow, currentState);
+        currentStateIndexer  = (currentStateIndexer + 1) % stateFlow.Length;
+        currentState = stateFlow[currentStateIndexer];
+
+        //Activate the newly switched to state's function
+
+        Action whichAction = stateToFunction[currentState];
+        whichAction.Invoke();
+       
+
+    }
+
+
+    
+    public void StartRails()
+    {
+        Debug.Log("Now on Rails");
+        RailStarted.Invoke();
+
+        
+    }
+
+    public void EndRails()
+    {
+        RailEnded.Invoke();
+    }
+    
+    public void StartEncounter()
+    {
+        Debug.Log("Now on Encounter");
+        EncounterStarted.Invoke();
+    }
+
+    public void EncounterEnd()
+    {
+        EncounterEnded.Invoke();
+    }
+
+
+    public void StartCleanup()
+    {
+        Debug.Log("Now on Cleanup");
+        CleanupStarted.Invoke();
+    }
+       
+    public void CleanupEnd()
+    {
+        CleanupEnded.Invoke();
+    }
+
+
+
+}
