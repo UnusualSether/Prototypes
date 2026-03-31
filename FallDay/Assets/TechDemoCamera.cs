3f2785569049d74f3c80f79e5e824752d0607ea5
Luiz Gustavo Oliveira <141835676+verumgus@users.noreply.github.com>
2026-03-31 08:07:09 -0300

    add_camController
    adicionado sistema controlador de camera

--- FallDay/Assets/TechDemoCamera.cs
+++ FallDay/Assets/TechDemoCamera.cs
@@ -11,20 +11,104 @@ using System.Runtime.CompilerServices;
 public class TechDemoCamera : MonoBehaviour
 {
   
-    public Transform toLookAt;
-    
+    [Header("Ajustes de Camera")]
+    [SerializeField] Transform _target;
+    [SerializeField] float _distanceFromTarget = 0;
+    [SerializeField] float _heightOffset = 2.0f;
+    [SerializeField] float _initialPitch = 20.0f;
+    [SerializeField] private float sensitivity = 10f;
+    [SerializeField] private float _maxRotation = 60f;
+    private float _yam = 0f;
+    private float _pitch = 0f;
 
-    
+    [Header("Ajustes de Touch cont")]
+    private float _touchTime = 0f;
+    private bool _isTouching = false;
+
+    public Vector2 inputDelta = Vector2.zero;
+
+
+
 
     public void Update()
     {
-        transform.LookAt(toLookAt.position);
+
+        //HandleInput();
+        TouchCamAtive();
+
+        Quaternion yamRotation = Quaternion.Euler(_initialPitch, _yam, 0f);
+
+        RotateCamera(yamRotation);
         CamControls();
-        if (HasCameraMoved(cameraCachedPos,Camera.main.transform.position) || HasPlayerMoved(playerCachedPos, toLookAt.transform.position))
+        if (HasCameraMoved(cameraCachedPos,Camera.main.transform.position) || HasPlayerMoved(playerCachedPos, _target.transform.position))
         {
             WallDisable();
         }
     }
+    public void TouchCamAtive()
+    {
+
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            inputDelta = touch.deltaPosition * 0.1f;
+            //condicional de pressionar
+            if (touch.phase == TouchPhase.Began)
+            {
+                _isTouching = true;
+                _touchTime = 0f;
+            }
+            if (_isTouching)
+            {
+                _touchTime += Time.deltaTime;
+                Debug.Log("Tempo pressionado:" + _touchTime.ToString("F2") + "s");
+            }
+            if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled)
+            {
+                Debug.Log("Toque finalizado. Tempo total:" + _touchTime.ToString("F2") + "s");
+                _isTouching = false;
+            }
+
+        }
+
+        //definição de ativação de camera, controlle o time para aticar a camera.
+        if (_touchTime > 1f)
+        {
+            HandleInput();
+        }
+
+    }
+    public float GetCurretyTouchTime()
+    {
+        return _touchTime;
+    }
+
+    public void HandleInput()
+    {
+
+        _yam += inputDelta.x * sensitivity * Time.deltaTime;
+        //_pitch -= inputDelta.y * sensitivity * Time.deltaTime;
+        // não precisa desse codigo se for deixar o jogador modificar a pisção da camera
+        _yam = Mathf.Clamp(_yam, -_maxRotation, _maxRotation);
+        // _pitch = Mathf.Clamp(_yam, -_maxRotation, _maxRotation);
+
+    }
+
+    void RotateCamera(Quaternion rotation)
+    {
+
+        Vector3 targetPositionWithHeight = _target.position + Vector3.up * _heightOffset;
+        Vector3 positionOffset = rotation * new Vector3(0, 0, -_distanceFromTarget);
+        transform.position = targetPositionWithHeight + positionOffset;
+        transform.rotation = rotation;
+        /*
+        Vector3 positionOffset = rotation * new Vector3(0, 0, -_distanceFromTarget);
+        transform.position = _target.position + positionOffset;
+        transform.rotation = rotation;
+        */
+    }
+
 
     private void CamControls()
     {
@@ -90,7 +174,7 @@ public class TechDemoCamera : MonoBehaviour
         RaycastHit[] hitObject;
         float sphereCastSize = 0.1f;
 
-        float maxwallSeeDistance = Vector3.Distance(transform.position, toLookAt.position);
+        float maxwallSeeDistance = Vector3.Distance(transform.position, _target.position);
 
 
         hitObject = Physics.SphereCastAll(rayOrigin,sphereCastSize, rayDirection, maxwallSeeDistance);
