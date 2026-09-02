68ef21509c0f55ba5e7dced85ad4bc15c6dc1f54
RLH107 <lucas.hellner08@gmail.com>
2026-09-02 09:37:15 -0300

    k

--- FallDay/Assets/C#Scripts/3DPathfinding/PathFolower.cs
+++ FallDay/Assets/C#Scripts/3DPathfinding/PathFolower.cs
@@ -4,8 +4,33 @@ using UnityEngine;
 public class PathFolower : MonoBehaviour
 {
     private List<Vector3> path;
+    bool isPathAtEnd;
     public void SetNewPath(List<Vector3> newPath)
     {
         path = newPath;
+    }
+
+    public void canWalk()
+    {
+        //is pos of curerent Object near start pos?
+        //true - start path
+        //false - serch new path
+        if (path != null && path.Count > 0)
+        {
+            if ((gameObject.transform.position - path[0]).magnitude < 0.2f) 
+            {
+                //is close enoth
+                //start walk
+
+            }
+            else
+            {
+                //Call a new pathfinding
+            }
+        }
+    }
+    public void walkPath()
+    {
+        //while()
     }
 }
