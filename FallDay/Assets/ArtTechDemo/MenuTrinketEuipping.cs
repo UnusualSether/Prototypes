74780b2f1c7d10f8ec927fed0e1a3e5913f9b0e0
Pedro Marcondes <141836243+Light212003@users.noreply.github.com>
2026-09-19 14:51:11 -0300

    Trinkets Display Fixed

--- FallDay/Assets/ArtTechDemo/MenuTrinketEuipping.cs
+++ FallDay/Assets/ArtTechDemo/MenuTrinketEuipping.cs
@@ -54,6 +54,12 @@ public class MenuTrinketEquipping : MonoBehaviour
     {
         var trinket_display = trinket_element_template.Instantiate();
 
+        var trinketimage = trinket_display.Q<Image>("trinket_sprite");
+
+        TrinketVisual.IMG(trinketimage, trinket_to_display);
+
+        //trinket_display.Q<Image>("trinket_sprite").sprite = trinket_to_display.trinket_sprite;
+
         trinket_display.Q<Label>("trinket_name").text = trinket_to_display.trinket_name;
 
         trinket_display.Q<Label>("trinket_desc").text = trinket_to_display.trinket_description;
